eaf7e918046d39d3d57b06891e927657cabf1195
Robbin <rsisimit-2023485@kinal.edu.gt>
2025-02-18 23:31:42 -0600

    montar servidor y conectar base de datos

---
+++ configs/server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { dbConnection } from './mongo.js';
+
+const configurarMiddlewares = (app) => {
+    app.use(express.urlencoded({extended: false}));
+    app.use(cors());
+    app.use(express.json());
+    app.use(helmet());
+    app.use(morgan('dev'));
+}
+
+const conectarDB = async () => {
+    try {
+        await dbConnection();
+        console.log("Conexion Exitosa Con La Base De Datos");
+    } catch (error) {
+        console.log("Error Al Conectar Con La Base De Datos", error);
+    }
+}
+
+export const iniciarServidor = async () => {
+    const app = express();
+    const port = process.env.PORT || 3010;
+
+    await conectarDB();
+    configurarMiddlewares(app);
+
+    app.listen(port, () => {
+        console.log(`Server Running On Port ${port}`);
+    });
+}
